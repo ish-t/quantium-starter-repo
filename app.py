52f26bc1de5c5918a992d3e0ec75bf9caa1360c5
ish-t <59807213+ish-t@users.noreply.github.com>
2026-04-17 07:36:52 +0100

    add region radio button and styling: task 4

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, callback, Input, Output
 import plotly.express as px
 import pandas as pd
 
@@ -6,14 +6,34 @@ app = Dash()
 
 df = pd.read_csv("./out/output.csv")
 
-fig = px.line(df, x="date", y="sales", title="Pink Morsel Sales by Date")
+regions = df["region"].unique()
 
 app.layout = html.Div(children=[
-    dcc.Graph(
-        id='graph',
-        figure=fig
+    html.H1("Pink Morsel Data",id="title"),
+    dcc.Graph(id="pink-morsel-line-graph"),
+    html.Div(id="regions",children=[
+    html.H2('Regions'),
+    dcc.RadioItems(
+        regions,
+        "north",
+        id="region-radio"
     )
 ])
 
-if __name__ == '__main__':
+])
+
+@callback(
+    Output("pink-morsel-line-graph", "figure"),
+    Input("region-radio", "value")
+)
+def graph_region_update(selected_region):
+
+    #filter dataframe by region
+    df_regions = df[df["region"] == selected_region]
+    #create new line graph
+    fig = px.line(df_regions, x="date", y="sales", title="Pink Morsel Sales by Date")
+    #return new line graph
+    return fig
+
+if __name__ == "__main__":
     app.run(debug=True)
